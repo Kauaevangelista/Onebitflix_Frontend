fda3338805907d74c4809d09114d6d86071c2f48
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-27 09:51:41 -0300

    Pastas e Componentes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Head from "next/head";
-import styles from '../styles/HomeNoAuth.module.scss'
+import styles from "../styles/homeNoAuth.module.scss" 
 
 const HomeNoAuth = function () {
   return (
-		<>
-			<Head>
-        <title>Home</title>
+    <>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+				<meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
       </Head>
-			<main></main>
-		</>
-  )
+      <main></main>
+    </>
+  );
 };
 
 export default HomeNoAuth;
