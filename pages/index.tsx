545d97cdd979064f7696d8dac493f294f52e4ad5
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-27 10:15:21 -0300

    Criação do header da página homeNoAuth - Header Não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/homeNoAuth.module.scss" 
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = function () {
   return (
@@ -10,7 +11,9 @@ const HomeNoAuth = function () {
 				<meta property="og:title" content="Onebitflix" key="title" />
 				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
       </Head>
-      <main></main>
+      <main>
+      <HeaderNoAuth />
+      </main>
     </>
   );
 };
