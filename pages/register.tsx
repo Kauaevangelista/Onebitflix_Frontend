f60995d4c83e4e9375f6d2b354aa9d5876c67d6a
Kauaevangelista <kaua13evangelista@gmail.com>
2023-04-29 08:13:15 -0300

    Conexão com o backend na parte de registro, criação do toast e configuração das mensagens de erro. - Registro

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,10 +1,51 @@
-import HeaderGeneric from "@/src/components/common/headerGeneric";
-import styles from "../styles/registerLogin.module.scss";
-import Head from "next/head";
+import HeaderGeneric from "@/src/components/common/headerGeneric"
+import styles from "../styles/registerLogin.module.scss"
+import Head from "next/head"
 import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap'
-import Footer from "@/src/components/common/footer";
+import Footer from "@/src/components/common/footer"
+import { FormEvent, useState } from "react"
+import authService from "@/src/services/authService"
+import { useRouter } from "next/router";
+import ToastComponent from "@/src/components/common/toast"
 
 const Register = function () {
+const router = useRouter();
+const [toastIsOpen, setToastIsOpen] = useState(false);
+const [toastMessage, setToastMessage] = useState("");
+const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+	event.preventDefault()
+
+  const formData = new FormData(event.currentTarget)
+  const firstName = formData.get("firstName")!.toString()
+  const lastName = formData.get("lastName")!.toString()
+  const phone = formData.get("phone")!.toString()
+  const birth = formData.get("birth")!.toString()
+  const email = formData.get("email")!.toString()
+  const password = formData.get("password")!.toString()
+  const confirmPassword = formData.get("confirmPassword")!.toString()
+  const params = { firstName, lastName, phone, birth, email, password }
+
+  if (password != confirmPassword) {
+    setToastIsOpen(true);
+    setTimeout(() => {
+    setToastIsOpen(false);
+  }, 1000 * 3);
+    setToastMessage("A senha e confirmação de senha são diferentes!");
+    return;
+  }
+
+  const { data, status } = await authService.register(params);
+
+  if (status === 201) {
+    router.push("/login?registred=true");
+  } else {
+    setToastIsOpen(true);
+    setTimeout(() => {
+    setToastIsOpen(false);
+  }, 1000 * 3);
+    setToastMessage(data.message);
+  }}
+
   return (
     <>
 		  <Head>
@@ -16,7 +57,7 @@ const Register = function () {
 	<HeaderGeneric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login"/>
 	<Container className="py-5">
 		<p className={styles.formTitle}>Bem-vindo(a) ao <span>OneBitFlix!</span></p>
-    <Form className={styles.form}>
+    <Form className={styles.form} onSubmit={handleRegister}>
 	<p className="text-center">
 		<strong>Faça a sua conta!</strong>
 	</p>
@@ -125,9 +166,10 @@ const Register = function () {
 </Form>
 </Container>
 <Footer />
-	</main>
+<ToastComponent color="bg-danger" isOpen={toastIsOpen} message={toastMessage}/>
+</main>
 </>
-  );
-};
+)
+}
 
-export default Register;
+export default Register
--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = function () {
     <>
 	<Container className={styles.footer}>
 	  <img src="/logoOnebitcode.svg" alt="logoFooter" className={styles.footerLogo} />
-      <a href="https://onebitcode.com" target={"blank"} className={styles.footerLink}>ONEBITCODE.COM</a>
+      <a href="https://www.linkedin.com/in/kauaevangelista/" target={"blank"} className={styles.footerLink}>SEUSITEAQUI.COM</a>
     </Container>
     <Container className={styles.git}>
     <p>Feito com ❤ por <a href="https://github.com/kauaevangelista" target={"blank"} className={styles.linkGit}>Kauã Evangelista</a></p>
