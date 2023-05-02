7a8eefc3a6be6b0c435ec379068b2ce557ebe54c
Kauaevangelista <kaua13evangelista@gmail.com>
2023-05-02 17:17:36 -0300

    Conexão do backend relacionado aos dados do usuário. Criação de um serviço e também de um método e também de toda a parte de conexão com o front - Conexão backend dados do usuário

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -2,8 +2,9 @@ import { Container, Form, Input } from "reactstrap";
 import styles from "./styles.module.scss";
 import Link from "next/link";
 import Modal from "react-modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import profileService from "@/src/services/profileService";
 
 Modal.setAppElement("#__next");
 
@@ -17,6 +18,14 @@ const HeaderAuth = function () {
 	};
 
 	const [modalOpen, setModalOpen] = useState(false);
+	const [initials, setInitials] = useState("");
+	useEffect(() => {
+		profileService.fetchCurrent().then((user) => {
+		  const firstNameInitial = user.firstName.slice(0, 1);
+		const lastNameInitial = user.lastName.slice(0, 1);
+		setInitials(firstNameInitial + lastNameInitial);
+	  });
+	}, []);
 
 	const handleOpenModal = () => setModalOpen(true)
 
@@ -42,7 +51,7 @@ const handleCloseModal = () => setModalOpen(false)
 	       alt="lupaHeader"
 	       className={styles.searchImg}
 	     />
-		<p className={styles.userProfile} onClick={handleOpenModal}>AB</p>
+		<p className={styles.userProfile} onClick={handleOpenModal}>{initials}</p>
      </div>
 	 <Modal
 		isOpen={modalOpen}
--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,17 +1,78 @@
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../../styles/profile.module.scss";
+import profileService from "@/src/services/profileService";
+import { useEffect, useState, FormEvent } from 'react'
+import ToastComponent from "../../common/toast";
+import { useRouter } from "next/router";
 
 const UserForm = function () {
+  const router = useRouter()
+
+  const [color, setColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [initialEmail, setInitialEmail] = useState(email)
+  const [created_at, setCreated_at] = useState("")
+
+  const date = new Date(created_at);
+  const month = date.toLocaleDateString("default", { month: "long" });
+
+  useEffect(() => {
+    profileService.fetchCurrent().then((user) => {
+      console.log(user)
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setPhone(user.phone);
+      setEmail(user.email);
+      setInitialEmail(user.email);
+      setCreated_at(user.createdAt);
+    });
+  }, []);
+
+  const handleUserUpdate = async function (event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  
+    const res = await profileService.userUpdate({
+      firstName,
+      lastName,
+      phone,
+      email,
+      created_at,
+    });
+  
+    if (res === 200) {
+      setToastIsOpen(true);
+      setErrorMessage("Informações alteradas com sucesso!");
+      setColor("bg-success");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+      if (email != initialEmail) {
+        sessionStorage.clear()
+        router.push('/')
+      }
+    } else {
+      setToastIsOpen(true);
+      setErrorMessage("Você não pode mudar para esse email!");
+      setColor("bg-danger");
+      setTimeout(() => setToastIsOpen(false), 1000 * 3);
+    }
+  };
+
   return (
     <>
-	<Form className={styles.form}>
+	<Form onSubmit={handleUserUpdate} className={styles.form}>
 	  <div className={styles.formName}>
-	    <p className={styles.nameAbbreviation}>NT</p>
-      <p className={styles.userName}>NAME TEST</p>
+	    <p className={styles.nameAbbreviation}>{ firstName.slice(0, 1)}
+    {lastName.slice(0, 1)}</p>
+      <p className={styles.userName}> {`${firstName} ${lastName}`}</p>
     </div>
     <div className={styles.memberTime}>
 	    <img src="/profile/iconUserAccount.svg" alt="iconProfile" className={styles.memberTimeImg}/>
-	    <p className={styles.memberTimeText}>Membro desde <br /> 20 de Abril de 2020</p>
+	    <p className={styles.memberTimeText}>Membro desde <br /> {`${date.getDate()} de ${month} de ${date.getFullYear()}`}</p>
 		</div>
 
         <hr />
@@ -28,7 +89,8 @@ const UserForm = function () {
     required
     maxLength={20}
     className={styles.inputFlex}
-    value={"Name"}
+    value={firstName}
+    onChange={(event) => { setFirstName(event.target.value) }}
     />
   </FormGroup>
   <FormGroup>
@@ -43,7 +105,8 @@ const UserForm = function () {
     required
     maxLength={20}
     className={styles.inputFlex}
-    value={"Test"}
+    value={lastName}
+    onChange={(event) => { setLastName(event.target.value) }}
     />
   </FormGroup>
 </div>
@@ -59,7 +122,8 @@ const UserForm = function () {
     placeholder="(xx) 9xxxx-xxxx"
     required
     className={styles.input}
-    value={"+55 (21) 99999-9999"}
+    value={phone}
+    onChange={(event) => { setPhone(event.target.value) }}
     />
   </FormGroup>
   <FormGroup>
@@ -73,15 +137,17 @@ const UserForm = function () {
     placeholder="Coloque o seu email"
     required
     className={styles.input}
-    value={"testeemail@gmail.com"}
+    value={email}
+    onChange={(event) => { setEmail(event.target.value) }}
     />
   </FormGroup>
 
-  <Button className={styles.formBtn} outline>
+  <Button className={styles.formBtn} type="submit" outline>
 	  Salvar Alterações
   </Button>
 </div>
   </Form>
+  <ToastComponent color={color} isOpen={toastIsOpen} message={errorMessage}/>
 </>
   );
 };
