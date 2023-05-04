ab8996a1f38aa5be1fd0979e0723867d5ea219db
Kauaevangelista <kaua13evangelista@gmail.com>
2023-05-04 10:18:35 -0300

    Página de episódios finalizada

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -129,7 +129,7 @@ const CoursePage = function () {
   </p>
 ) : (
 	course?.episodes?.map((episode) => (
-	  <EpisodeList key={episode.id} episode={episode} />
+	  <EpisodeList key={episode.id} episode={episode} course={course} />
   ))
 )}  </p>
   </Container>
