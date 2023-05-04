515408c98b314b5fa052229eb6cfcb53b742c15f
Kauaevangelista <kaua13evangelista@gmail.com>
2023-05-04 08:37:14 -0300

    Criação dos elementos da página de curso, do seu estilo visual e também da sua responsividade completa. Feito também a conexão com o backend relacionado a curso individual - Página de curso

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -6,9 +6,15 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 import courseService, { CourseType } from "@/src/services/courseService";
+import { Button, Container } from "reactstrap";
+import PageSpinner from "@/src/components/common/spinner";
+import EpisodeList from "@/src/components/episodeList";
+import Footer from "@/src/components/common/footer";
 
 const CoursePage = function () {
   const [course, setCourse] = useState<CourseType>();
+  const [liked, setLiked] = useState(false);
+  const [favorited, setFavorited] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
@@ -19,6 +25,8 @@ const CoursePage = function () {
   
     if (res.status === 200) {
       setCourse(res.data);
+      setLiked(res.data.liked);
+      setFavorited(res.data.favorited);
     }
   };
 
@@ -26,17 +34,108 @@ const CoursePage = function () {
     getCourse();
   }, [id]);
 
+  const handleLikeCourse = async () => {
+    if (typeof id !== 'string') return
+
+    if (liked === true) {
+        await courseService.removeLike(id);
+        setLiked(false);
+    } else {
+        await courseService.like(id);
+        setLiked(true);
+    }
+  };
+
+  const handleFavCourse = async () => {
+    if (typeof id !== 'string') return
+
+    if (favorited === true) {
+      await courseService.removeFav(id);
+      setFavorited(false);
+    } else {
+      await courseService.addToFav(id);
+      setFavorited(true);
+    }
+  };
+
+  if (course === undefined) return <PageSpinner/>
+
   return (
     <>
       <Head>
         <title>Onebitflix - {course?.name}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
-		<HeaderAuth />
-    <p>{course?.name}</p>
-	</main>
-    </>
+    <main>
+      <div
+        style={{
+        backgroundImage: `linear-gradient(to bottom, #6666661a, #151515),
+        url(${process.env.NEXT_PUBLIC_BASEURL}/${course?.thumbnailUrl})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "450px",
+  }}>
+	  <HeaderAuth />
+      </div>
+  <Container className={styles.courseInfo}>
+	  <p className={styles.courseTitle}>{course?.name}</p>
+    <p className={styles.courseDescription}>{course?.synopsis}</p>
+    <Button outline className={styles.courseBtn}  disabled={course?.episodes?.length === 0 ? true : false}>
+	    ASSISTIR AGORA!
+      <img
+      src="/buttonPlay.svg"
+      alt="buttonImg"
+      className={styles.buttonImg}/>
+    </Button>
+	  <div className={styles.interactions}>
+    {liked === false ? (
+	<img
+	  src="/course/iconLike.svg"
+    alt="likeImage"
+    className={styles.interactionImages}
+    onClick={handleLikeCourse}
+  />
+) : (
+	<img
+	  src="/course/iconLiked.svg"
+    alt="likedImage"
+    className={styles.interactionImages}
+    onClick={handleLikeCourse}
+  />
+)}     
+{favorited === false ? (
+	<img
+	  onClick={handleFavCourse}
+    src="/course/iconAddFav.svg"
+    alt="addFav"
+    className={styles.interactionImages}
+  />
+) : (
+	<img
+	  onClick={handleFavCourse}
+    src="/course/iconFavorited.svg"
+    alt="favorited"
+    className={styles.interactionImages}
+	/>
+)}
+	  </div>
+  </Container>
+  <Container className={styles.episodeInfo}>
+  <p className={styles.episodeDivision}>EPISÓDIOS</p>
+  <p className={styles.episodeLength}>
+  {course?.episodes?.length === 0 ? (
+	<p>
+	  <strong>Não temos episódios ainda, volte outra hora! &#x1F606;&#x1F918;</strong>
+  </p>
+) : (
+	course?.episodes?.map((episode) => (
+	  <EpisodeList key={episode.id} episode={episode} />
+  ))
+)}  </p>
+  </Container>
+  <Footer/>
+</main>
+</>
   );
 };
 
